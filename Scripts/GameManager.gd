56c398a9e764188ea9f5f858c95f94660326d956
EmreDogann <emre.dogan.1@protonmail.com>
2022-06-03 18:55:56 +0100

    Reorganized post process effects.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,13 +1,18 @@
 extends Node
 
-#var directoryHandlerClass = load("res://FileTree/DirectoryHandler.cs")
 onready var fileSystem = get_tree().root.get_node("Node2D/CanvasLayer/FileExplorer/Window/VBoxContainer/Body/MarginContainer/VBoxContainer/Files")
-var fileItem
+onready var blankWindow = preload("res://Scenes/BlankWindow.tscn")
+
+var current_level: int = 0
+
+var rng = RandomNumberGenerator.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Call to add virus. if hidden is true, name of virus may be blank.
 #	fileSystem.connect("virus_deleted", self, "next_level")
-	pass
+	
+	rng.randomize()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -18,3 +23,6 @@ func next_level():
 
 func virus_deleted():
 	next_level()
+
+func spawn_window() -> void:
+	pass
