761b9f6f200d26130ae8435b15ce1f294cbf8df0
aum <imaum2208@gmail.com>
2022-06-03 01:59:53 +0100

    Fiddling with Signals Virus Deleted

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,12 +1,15 @@
 extends Node
+
+
+
 #var directoryHandlerClass = load("res://FileTree/DirectoryHandler.cs")
 onready var fileSystem = get_tree().root.get_node("Node2D/CanvasLayer/FileExplorer/Window/VBoxContainer/Body/MarginContainer/VBoxContainer/Files")
 var fileItem
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# call to add virus. if hidden is true, name of virus may be blank
-	fileItem = fileSystem.addVirusRandomly("Level1Virus", false)
-	fileSystem.connect("virus_deleted", self, "next_level")
+	#fileItem = fileSystem.addVirusRandomly("Level1Virus", false)
+	#fileSystem.connect("virus_deleted", self, "next_level")
 	pass
 
 
@@ -14,6 +17,8 @@ func _ready():
 #func _process(delta):
 #pass
 
-
 func next_level():
 	print("Load next level...")
+
+func virus_deleted():
+	next_level()
